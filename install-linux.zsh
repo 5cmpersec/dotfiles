e8307d745347b1fee4a654ced791e20454a4c31e
Vu Thanh Cong <vuthanhcong.ict@gmail.com>
2022-09-05 09:38:51 +0800

    Update config for new desktop

--- install-linux.zsh
+++ install-linux.zsh
@@ -3,5 +3,10 @@
 set -eu
 
 cwd=$(pwd)
-ln -sf ${cwd}/config/vim/vimrc ~/.vimrc
+ln -sf ${cwd}/config/vim/vimrc ~/.vim/vimrc
+
+echo "Configure tmux"
+mkdir -p ~/.config/tmux/plugins/
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+ln -sf ${cwd}/config/tmux/tmux.conf ~/.config/tmux/tmux.conf
 
